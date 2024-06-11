ae15a6c104350563a8f69ae2f95f6157aa37be82
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-11 14:34:03 -0300

    Métodos

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -7,5 +7,29 @@ public class Cachorro {
     public int altura;
     public double peso;
     public int tamanhoDoRabo;
+    public String estadoDeEspirito;
+
+    public void comer() {
+    }
+
+    public void latir() {
+        System.out.println("Au, Au, Au");
+    }
+
+    public String pegar() {
+        return "bolinha";
+    }
+
+    public String interagir(String acao) {
+        if (acao.equals("carinho")) {
+            this.estadoDeEspirito = "feliz";
+        } else if (acao.equals("vai dormir")) {
+            this.estadoDeEspirito = "bravo";
+        } else {
+            this.estadoDeEspirito = "neutro";
+        }
+
+        return estadoDeEspirito;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        * byte	  -128 to 127
-        * short   -32,768 to 32,767
-        * int     -2,147,483,648 to 2,147,483,647
-        * long    -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-        * float   Sufficient for storing 6 to 7 decimal digits
-        * double	Sufficient for storing 15 decimal digits
-        * char	Stores a single character/letter or ASCII values
-        * boolean	true or false
-        * */
+         * byte	  -128 to 127
+         * short   -32,768 to 32,767
+         * int     -2,147,483,648 to 2,147,483,647
+         * long    -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+         * float   Sufficient for storing 6 to 7 decimal digits
+         * double	Sufficient for storing 15 decimal digits
+         * char	Stores a single character/letter or ASCII values
+         * boolean	true or false
+         * */
 
         Cachorro cachorro1 = new Cachorro();
 
@@ -23,7 +23,11 @@ public class Main {
         cachorro1.peso = 5.5;
         cachorro1.tamanhoDoRabo = 5;
 
-        System.out.println(cachorro1);
+//        cachorro1.latir(); // void
+//        System.out.println("O cachorro pegou uma " + cachorro1.pegar() + "."); // return string
 
+        System.out.println("O cachorro está " + cachorro1.interagir("carinho") + ".");
+        System.out.println("O cachorro está " + cachorro1.interagir("nada") + ".");
+        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir") + ".");
     }
 }
