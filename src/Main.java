09539468ae2b26e9b57340f6fdb70435083e66cb
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-11 11:40:57 -0300

    Hello Word com Java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        * byte	  -128 to 127
+        * short   -32,768 to 32,767
+        * int     -2,147,483,648 to 2,147,483,647
+        * long    -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        * float   Sufficient for storing 6 to 7 decimal digits
+        * double	Sufficient for storing 15 decimal digits
+        * char	Stores a single character/letter or ASCII values
+        * boolean	true or false
+        * */
+
+        byte varByte = 120;
+        System.out.println(varByte);
+
+        int varInt = 11222333;
+        System.out.println(varInt);
+    }
+}
